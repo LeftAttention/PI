74a338027e3d45b7b4acf96dfb371b4c7357f6b3
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-01-21 22:46:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -74,3 +74,11 @@ def Testing():
                 break
         cap.release()
         cv2.destroyAllWindows()
+	
+    elif p.mode == 2: # check model with a picture
+        test_image = cv2.imread(p.test_root_url+"clips/0530/1492720840345996040_0/20.jpg")
+        test_image = cv2.resize(test_image, (512,256))/255.0
+        test_image = np.rollaxis(test_image, axis=2, start=0)
+        _, _, ti = test(lane_agent, np.array([test_image]))
+        cv2.imshow("test", ti[0])
+        cv2.waitKey(0)   
