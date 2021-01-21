857d290c63d6f51ca31e05057c0749b4974b25c0
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-01-21 22:48:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -145,3 +145,48 @@ def eliminate_fewer_points(x, y):
             out_x.append(i)
             out_y.append(j)     
     return out_x, out_y
+
+############################################################################
+## generate raw output
+############################################################################
+def generate_result(confidance, offsets,instance, thresh):
+
+    mask = confidance > thresh
+
+    grid = p.grid_location[mask]
+    offset = offsets[mask]
+    feature = instance[mask]
+
+    lane_feature = []
+    x = []
+    y = []
+    for i in range(len(grid)):
+        if (np.sum(feature[i]**2))>=0:
+            point_x = int((offset[i][0]+grid[i][0])*p.resize_ratio)
+            point_y = int((offset[i][1]+grid[i][1])*p.resize_ratio)
+            if point_x > p.x_size or point_x < 0 or point_y > p.y_size or point_y < 0:
+                continue
+            if len(lane_feature) == 0:
+                lane_feature.append(feature[i])
+                x.append([point_x])
+                y.append([point_y])
+            else:
+                flag = 0
+                index = 0
+                min_feature_index = -1
+                min_feature_dis = 10000
+                for feature_idx, j in enumerate(lane_feature):
+                    dis = np.linalg.norm((feature[i] - j)**2)
+                    if min_feature_dis > dis:
+                        min_feature_dis = dis
+                        min_feature_index = feature_idx
+                if min_feature_dis <= p.threshold_instance:
+                    lane_feature[min_feature_index] = (lane_feature[min_feature_index]*len(x[min_feature_index]) + feature[i])/(len(x[min_feature_index])+1)
+                    x[min_feature_index].append(point_x)
+                    y[min_feature_index].append(point_y)
+                elif len(lane_feature) < 12:
+                    lane_feature.append(feature[i])
+                    x.append([point_x])
+                    y.append([point_y])
+                
+    return x, y
