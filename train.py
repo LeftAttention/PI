691e2346a0ab5757944629dfc6926e5e492fe5b0
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-01-21 23:35:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -65,3 +65,29 @@ def Training():
     print('Training loop')
     step = 0
     sampling_list = None
+    
+    for epoch in range(p.n_epoch):
+        lane_agent.training_mode()
+        for inputs, target_lanes, target_h, test_image, data_list in loader.Generate(sampling_list):
+            #training
+            #util.visualize_points(inputs[0], target_lanes[0], target_h[0])
+            print("epoch : " + str(epoch))
+            print("step : " + str(step))
+            loss_p = lane_agent.train(inputs, target_lanes, target_h, epoch, lane_agent, data_list)
+            torch.cuda.synchronize()
+            loss_p = loss_p.cpu().data
+            
+            if step%50 == 0:
+                vis.line(
+                    X=torch.ones((1, 1)).cpu() * int(step/50),
+                    Y=torch.Tensor([loss_p]).unsqueeze(0).cpu(),
+                    win=loss_window,
+                    update='append')
+                
+            if step%100 == 0:
+                lane_agent.save_model(int(step/100), loss_p)
+                testing(lane_agent, test_image, step, loss_p)
+            step += 1
+
+        sampling_list = copy.deepcopy(lane_agent.get_data_list())
+        lane_agent.sample_reset()
