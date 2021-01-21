094d836c1db2917fd6255f6b22de442eec00bbf9
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-01-21 23:34:23 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,52 @@
+import cv2
+import torch
+import visdom
+#import sys
 
+import Assistant
+import numpy as np
+from data_loader import Generator
+from parameters import Parameters
+import test
+import evaluation
+import util
+import copy
+
+p = Parameters()
+
+###############################################################
+##
+## Training
+## 
+###############################################################
+def Training():
+    print('Training')
+
+    ####################################################################
+    ## Hyper parameter
+    ####################################################################
+    print('Initializing hyper parameter')
+
+    vis = visdom.Visdom()
+    loss_window = vis.line(X=torch.zeros((1,)).cpu(),
+                           Y=torch.zeros((1)).cpu(),
+                           opts=dict(xlabel='epoch',
+                                     ylabel='Loss',
+                                     title='Training Loss',
+                                     legend=['Loss']))
+    
+    #########################################################################
+    ## Get dataset
+    #########################################################################
+    print("Get dataset")
+    loader = Generator()
+
+    ##############################
+    ## Get agent and model
+    ##############################
+    print('Get agent')
+    if p.model_path == "":
+        lane_agent = agent.Agent()
+    else:
+        lane_agent = agent.Agent()
+        lane_agent.load_weights(1912, "tensor(0.9420)")
