e648e1513bfdb4c4f814688155dbf89e8e62c463
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-01-21 22:30:09 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,20 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import ujson as json
+
+
+class LaneEval(object):
+    lr = LinearRegression()
+    pixel_thresh = 20
+    pt_thresh = 0.85
+
+    @staticmethod
+    def get_angle(xs, y_samples):
+        xs, ys = xs[xs >= 0], y_samples[xs >= 0]
+        if len(xs) > 1:
+            LaneEval.lr.fit(ys[:, None], xs)
+            k = LaneEval.lr.coef_[0]
+            theta = np.arctan(k)
+        else:
+            theta = 0
+        return theta
